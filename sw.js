9d8087b266ea1c2ca2a781a06b000459303f2eef
TCKACOSKUN <kagan.coskun@atmosware.turkcell.com.tr>
2025-05-30 01:56:44 +0300

    sw upd

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.addEventListener("push", e => {
   console.log("event", e);
   console.log("event.data", e.data);
   console.log("event.data.text", e.data.text());
+  console.log("event.data.text", e.data.json());
 
   const config = {
     body: "Mesaj içeriği burada yer alacak.",

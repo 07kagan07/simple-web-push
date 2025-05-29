42779320722341cfc3f0d42c3266644d62634036
TCKACOSKUN <kagan.coskun@atmosware.turkcell.com.tr>
2025-05-30 01:47:26 +0300

    sw upd

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-self.addEventListener("push", (e)=>{
-
+self.addEventListener("push", e => {
   const config = {
-    body: "This is a push notification",
+    body: "Mesaj içeriği burada yer alacak.",
     data: {
       dateOfArrival: Date.now(),
       primaryKey: 1
     }
-  }
+  };
 
   if (self.registration) {
-    e.waitUntil(self.registration.showNotification("Push Notification", config));
+    e.waitUntil(self.registration.showNotification("Mesaj başlığı burada", config));
   } else {
     console.error("Service worker registration not found.");
   }
-})
+});
